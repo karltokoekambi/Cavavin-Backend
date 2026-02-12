db732cd5cc4992203e0a1bdb6cd3aaaf2a42707b
karltokoekambi <acamer.guillaume@gmail.com>
2026-02-12 12:36:07 +0100

    initial data seed

--- Cavavin.API/Data/AppDbContext.cs
+++ Cavavin.API/Data/AppDbContext.cs
@@ -27,5 +27,64 @@ public class AppDbContext : DbContext
                 v => v.ToString().ToLower(),
                 v => (WineRegion)Enum.Parse(typeof(WineRegion), v, true)
             );
+
+        modelBuilder.Entity<WineBottle>().HasData(
+            new WineBottle
+            {
+                Id = 1,
+                Name = "Chateau Margaux",
+                Domain = "Chateau Margaux",
+                Vintage = 2015,
+                StartMaturity = 2025,
+                EndMaturity = 2050,
+                Region = WineRegion.Bordeaux,
+                Type = WineType.Red,
+                FoodPairingKeywords = "agneau,gibier,boeuf",
+                IsFavorite = true,
+                Quantity = 3
+            },
+            new WineBottle
+            {
+                Id = 2,
+                Name = "Cuvee des Enchanteleurs",
+                Domain = "Mailly Grand Cru",
+                Vintage = 2012,
+                StartMaturity = 2020,
+                EndMaturity = 2035,
+                Region = WineRegion.Champagne,
+                Type = WineType.Sparkling,
+                FoodPairingKeywords = "aperitif,huitres,crevettes",
+                IsFavorite = false,
+                Quantity = 6
+            },
+            new WineBottle
+            {
+                Id = 3,
+                Name = "Sancerre d'Antan",
+                Domain = "Henri Bourgeois",
+                Vintage = 2021,
+                StartMaturity = 2023,
+                EndMaturity = 2028,
+                Region = WineRegion.Loire,
+                Type = WineType.White,
+                FoodPairingKeywords = "poisson,chevre,volaille",
+                IsFavorite = true,
+                Quantity = 4
+            },
+            new WineBottle
+            {
+                Id = 4,
+                Name = "Cote Rotie La Mouline",
+                Domain = "E. Guigal",
+                Vintage = 2018,
+                StartMaturity = 2026,
+                EndMaturity = 2045,
+                Region = WineRegion.Rhone,
+                Type = WineType.Red,
+                FoodPairingKeywords = "viande rouge,agneau,truffes",
+                IsFavorite = true,
+                Quantity = 2
+            }
+            );
     }
 }
---
+++ Cavavin.API/Migrations/20260212113252_SeedInitialData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cavavin.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "wine_bottles",
+                columns: new[] { "id", "domain", "end_maturity", "food_pairing_keywords", "is_favorite", "name", "quantity", "region", "start_maturity", "type", "vintage" },
+                values: new object[,]
+                {
+                    { 1, "Chateau Margaux", 2050, "agneau,gibier,boeuf", true, "Chateau Margaux", 3, "bordeaux", 2025, "red", 2015 },
+                    { 2, "Mailly Grand Cru", 2035, "aperitif,huitres,crevettes", false, "Cuvee des Enchanteleurs", 6, "champagne", 2020, "sparkling", 2012 },
+                    { 3, "Henri Bourgeois", 2028, "poisson,chevre,volaille", true, "Sancerre d'Antan", 4, "loire", 2023, "white", 2021 },
+                    { 4, "E. Guigal", 2045, "viande rouge,agneau,truffes", true, "Cote Rotie La Mouline", 2, "rhone", 2026, "red", 2018 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "wine_bottles",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "wine_bottles",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "wine_bottles",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "wine_bottles",
+                keyColumn: "id",
+                keyValue: 4);
+        }
+    }
+}
